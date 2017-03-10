3ae8159d8252a9ff4255fbdead35fee4a357c14f
waterkh <pjclark@email.sc.edu>
2017-03-10 14:50:51 -0500

    First Commit + Google Maps [WIP] + NOAA Integration [WIP] + .gitignore
    + First commit
    + Google Maps is working on iOS but giving errors on Android. Next step
    will be to figure out what is causing errors and also add locations,
    such as stations and such from NOAA.
    + NOAA Integration has been started. Tested token and API call to
    receive stations and this worked. Class created for NOAA objects, not
    entirely sure if they are all the same or not though. This needs to be
    checked @WaterKH .
    + .gitignore file has been added to get rid of all .dll files, obj and
    bin folders, as well as packages.

---
+++ ClarkNOAA/Utilities/Globals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ClarkNOAA
+{
+	public class Globals
+	{
+		public static string NOAAKey = "yUQisAIPKtcNxRTxVJumPjRlNDZPudZa";
+		public static string NOAABaseURI = "https://www.ncdc.noaa.gov/cdo-web/api/v2/";
+
+		public static string AndroidGoogleMapsKey = "AIzaSyCWtshGLB6IBcaoNnOHchwggjeK0MOQ_b0";
+		public static string iOSGoogleMapsKey = "AIzaSyAKEsSLSp-14mZD3QoCkyg82IyhHT5Lmuc";
+	}
+}
---
+++ ClarkNOAA/Utilities/REST_API.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+
+namespace ClarkNOAA
+{
+	public class REST_API
+	{
+		public static void NOAA_GetStations()
+		{
+			HttpClient client = new HttpClient ();
+			client.BaseAddress = new Uri (Globals.NOAABaseURI);
+
+			client.DefaultRequestHeaders.Add ("token", Globals.NOAAKey);
+
+			// Add an Accept header for JSON format.
+			client.DefaultRequestHeaders.Accept.Add (new MediaTypeWithQualityHeaderValue ("application/json"));
+
+			string urlParameters = "stations";
+
+			// List data response.
+			HttpResponseMessage response = client.GetAsync (urlParameters).Result;  // Blocking call!
+
+			if (response.IsSuccessStatusCode) {
+				// Parse the response body. Blocking!
+				var dataObjects = response.Content.ReadAsAsync<NOAAItem> ().Result;
+
+				foreach(var r in dataObjects.results)
+				{
+					Debug.WriteLine ("{0}", r.name);	
+				}
+			} 
+			else 
+			{
+				Debug.WriteLine ("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+				Debug.WriteLine (client.BaseAddress + "" + urlParameters);
+			}
+		}
+	}
+}
